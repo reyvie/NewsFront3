39b5afeb155d4edbf38b382889e018a5f913150c
ReyvieB <reyvieb@admins-mac-mini.local>
2019-10-25 13:06:20 +0800

    Initial Commit

---
+++ NewsFront3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFront3
+//
+//  Created by ReyvieB on 10/25/19.
+//  Copyright © 2019 ReyvieB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
