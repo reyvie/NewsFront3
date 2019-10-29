82971f543234e4f93d50d8b1a7090e71c7a21a63
ReyvieB <reyvieb@admins-mac-mini.local>
2019-10-29 12:47:37 +0800

    Initial commit

--- NewsFront3/ViewController.swift
+++ NewsFront3/ViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var RegisterButton: UIButton!
+    @IBOutlet weak var EmailTextField: UITextField!
+    
+   
+    @IBAction func HideKeyboard(_ sender: Any) {
+        EmailTextField.resignFirstResponder()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let newLayer = CAGradientLayer()
+        newLayer.colors = [UIColor.darkGray, UIColor.blue.cgColor]
+        newLayer.frame = view.frame
+
+        view.layer.insertSublayer(newLayer, at: 1)
+        
+        RegisterButton.layer.cornerRadius = 3
+        EmailTextField.layer.cornerRadius = 3
+        EmailTextField.layer.borderWidth = 1.5;
+        EmailTextField.layer.borderColor = UIColor.white.cgColor
+        EmailTextField.attributedPlaceholder = NSAttributedString(string: "Email",
+                                                                  attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
     }
+
 
 
 }
